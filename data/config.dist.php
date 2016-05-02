65c093958849197ee402984da9533a7ee3288541
Diftraku <diftraku@gmail.com>
2016-05-03 01:06:06 +0300

    Updated defaults and dist config

--- data/config.dist.php
+++ data/config.dist.php
@@ -14,10 +14,16 @@ function _d($name, $value) {if(!defined($name)) define($name, $value);}
 _d('CF_DB_DSN',         'sqlite:' . CF_DIR_DATA . 'cfmanage2.db');  // PDO DSN
 _d('CF_DB_USERNAME',    '');                                        // Database username
 _d('CF_DB_PASSWORD',    '');                                        // Database password
+_d('CF_DB_TBL_PREFIX',  'cfm_');                                    // Database password
 _d('CF_VERSION',        '0.0.1');                                   // System version
 _d('CF_BUILD',          'DEV');                                     // Build type
 _d('CF_TZ',             'UTC');                                     // Default timezone
-_d('CF_HASH_ALGO',      'SHA256');                                  // Hashing algorithm
+_d('CF_HASH_ALGO',      'SHA256');                                  // Default hashing algorithm
+_d('CF_PASS_ALGO',      PASSWORD_BCRYPT);                           // Default password_hash() algorithm
+_d('CF_PROXY_CHECKIP',  true);                                      // Check for Proxy IPs
+
+_d('CF_AUTH_HTTP',  true);                                          // Should we use HTTP auth?
+_d('CF_AUTH_HTTP_REALM',  'CF-Manager 2');                          // HTTP Authentication realm
 
 // Default overrides for Slim Framework
 // These are merged in public/index.php before instantiation
@@ -32,7 +38,7 @@ $settings = [
 
         // Monolog settings
         'logger' => [
-            'name' => 'slim-app',
+            'name' => 'cfm',
             'path' => CF_DIR_LOGS . 'app.log',
         ],
     ],
--- data/defaults.php
+++ data/defaults.php
@@ -8,9 +8,17 @@
  */
 
 // Basic directories
-define ('CF_DIR_DATA', __DIR__ . '/../data/');
-define ('CF_DIR_LOGS', __DIR__ . '/../logs/');
-define ('CF_DIR_PUBLIC', __DIR__ . '/../public/');
-define ('CF_DIR_SRC', __DIR__ . '/../src/');
-define ('CF_DIR_TMPL', __DIR__ . '/../templates/');
-define ('CF_DIR_VEND', __DIR__ . '/../vendor/');
+define('CF_DIR_DATA', __DIR__ . '/../data/');
+define('CF_DIR_LOGS', __DIR__ . '/../logs/');
+define('CF_DIR_PUBLIC', __DIR__ . '/../public/');
+define('CF_DIR_SRC', __DIR__ . '/../src/');
+define('CF_DIR_TMPL', __DIR__ . '/../templates/');
+define('CF_DIR_VEND', __DIR__ . '/../vendor/');
+
+// Database table names
+define('CF_DB_TBL_USERS', 'users');
+define('CF_DB_TBL_TICKETS', 'tickets');
+define('CF_DB_TBL_EVENTS', 'events');
+
+// RedBean defaults
+define('REDBEAN_MODEL_PREFIX', '\\CFM2\\Models\\' );
