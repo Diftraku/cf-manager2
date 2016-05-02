81170d2b51a73e3ad47131724d1694efe9898a4e
Diftraku <diftraku@gmail.com>
2016-05-03 01:06:52 +0300

    Adding new Middleware and updating .htaccess for HTTP Auth headers

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,22 @@
 <?php
 // Application middleware
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+if (CF_PROXY_CHECKIP) {
+    $app->add(new RKA\Middleware\IpAddress(true));
+}
+
+if (CF_AUTH_HTTP) {
+    use Slim\Middleware\HttpBasicAuthentication\PdoAuthenticator;
+    use Slim\Middleware\HttpBasicAuthentication;
+
+    $app->add(new HttpBasicAuthentication([
+        'path' => '/*',
+        'realm' => CF_AUTH_HTTP_REALM,
+        'authenticator' => new PdoAuthenticator([
+            'pdo' => R::getPDO(),
+            'table' => CF_DB_TBL_PREFIX.CF_DB_TBL_USERS,
+            'user' => 'username',
+            'hash' => 'password'
+        ])
+    ]));
+}
