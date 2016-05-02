b792af2232a10692cfd8933e45be9efc3924011e
Diftraku <diftraku@gmail.com>
2016-05-03 01:08:13 +0300

    Adding User stub functions and model

--- src/CFM2/Controllers/User.php
+++ src/CFM2/Controllers/User.php
@@ -8,8 +8,80 @@
 
 namespace CFM2\Controllers;
 
+use Interop\Container\ContainerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 class User
 {
+    /**
+     * Slim Container
+     * @var ContainerInterface
+     */
+    protected $ci;
 
+    public function __construct(ContainerInterface $ci) {
+        $this->ci = $ci;
+    }
+
+    /**
+     * loginUser
+     * Process user login
+     * @method POST
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     */
+    public function loginUser( Request $request, Response $response, $args = [] ) {
+        
+
+    }
+
+    /**
+     * logoutUser
+     * Process user logout
+     * @method POST
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     */
+    public function logoutUser( Request $request, Response $response, $args = [] ) {
+
+    }
+
+    /**
+     * getUser
+     * Return user details
+     * @method GET
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     */
+    public function getUser( Request $request, Response $response, $args = [] ) {
+
+    }
+
+    /**
+     * updateUser
+     * Update user details
+     * @method PUT
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     */
+    public function updateUser( Request $request, Response $response, $args = [] ) {
+
+    }
+
+    /**
+     * createUser
+     * Create a new user
+     * @method POST
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     */
+    public function createUser( Request $request, Response $response, $args = [] ) {
+
+    }
 }
--- src/CFM2/Models/User.php
+++ src/CFM2/Models/User.php
@@ -11,7 +11,40 @@ namespace CFM2\Models;
 
 use RedBeanPHP\SimpleModel;
 
+/**
+ * Class User
+ * @package CFM2\Models
+ */
 class User extends SimpleModel
 {
+    /**
+     * dispense
+     * FUSE method for pre-loading RedBean
+     */
+    public function dispense() {
+        // This will most likely be a really bad way to do this
+        // @TODO Test this stuff
+        $this->bean->display_name = '';
+        $this->bean->username = '';
+        $this->bean->password = '';
+        $this->bean->email = '';
+        $this->bean->enabled = true;
+        $this->bean->role = 'user';
+        $this->bean->created_on = '';
+        $this->bean->created_by = '';
+        $this->bean->modified_on = '';
+        $this->bean->modified_by = '';
+        $this->bean->last_access = '';
+    }
+    /**
+     * update
+     * FUSE method for RedBean validation use
+     */
+    public function update() {
+        // Hash the password if it changes
+        if($this->bean->hasChanged('password')) {
+            $this->bean->password = password_hash($this->bean->password, CF_PASS_ALGO);
+        }
+    }
 
 }
