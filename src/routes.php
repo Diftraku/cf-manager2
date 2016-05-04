c6559c7a04ef96eb3475ef8f0e4a7bebad8eaf2f
Diftraku <diftraku@gmail.com>
2016-05-04 12:35:50 +0300

    Fixed up routing

--- src/routes.php
+++ src/routes.php
@@ -14,23 +14,23 @@ $app->get('/', function ($request, $response, $args) {
 
 $app->group('/ticket', function () {
     $this->get('', '\CFM2\Controllers\Ticket:getTickets');
-    $this->get('/{id:0-9+}', '\CFM2\Controllers\Ticket:getTicket');
-    $this->put('/{id:0-9+}', '\CFM2\Controllers\Ticket:updateTicket');
+    $this->get('/{id:[0-9]+}', '\CFM2\Controllers\Ticket:getTicket');
+    $this->put('/{id:[0-9]+}', '\CFM2\Controllers\Ticket:updateTicket');
     $this->post('', '\CFM2\Controllers\Ticket:createTicket');
 });
 $app->group('/user', function () {
     $this->get('', '\CFM2\Controllers\User:getUsers');
-    $this->get('/{id:0-9+}', '\CFM2\Controllers\User:getUser');
-    $this->put('/{id:0-9+}', '\CFM2\Controllers\User:updateUser');
+    $this->get('/{id:[0-9]+}', '\CFM2\Controllers\User:getUser');
+    $this->put('/{id:[0-9]+}', '\CFM2\Controllers\User:updateUser');
     $this->post('', '\CFM2\Controllers\User:createUser');
 });
 
 /*$app->group('/event', function () {
     $this->get('', '\CFM2\Controllers\Event:getEvents');
-    $this->get('/{id:0-9+}', '\CFM2\Controllers\Event:getEvent');
-    $this->put('/{id:0-9+}', '\CFM2\Controllers\Event:updateEvent');
+    $this->get('/{id:[0-9]+}', '\CFM2\Controllers\Event:getEvent');
+    $this->put('/{id:[0-9]+}', '\CFM2\Controllers\Event:updateEvent');
     $this->post('', '\CFM2\Controllers\Event:createEvent');
 });*/
-$app->any('/event{(|/*)}', function (Request $request, Response $response, $args = []) {
+$app->any('/event', function (Request $request, Response $response, $args = []) {
     $response->withStatus(501)->withJson(['message' => 'Not implemented']);
 });
