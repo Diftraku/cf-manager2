10d80bcc1d9d8072126fc3b0d7259f5387582740
Diftraku <diftraku@gmail.com>
2016-05-04 12:35:34 +0300

    Adding stub error handler

--- src/dependencies.php
+++ src/dependencies.php
@@ -14,6 +14,15 @@ $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::INFO));
     return $logger;
 };
+
+// error handling
+/*$container['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) use ($c) {
+        return $c['response']->withStatus(500)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Something went wrong!');
+    };
+};*/
